7389ff5f65330dd4e8fa63288325848679294811
James Felts <jfelts1@users.noreply.github.com>
2015-09-28 13:06:33 -0700

    main

---
+++ Assign2Minesweeper/Assign2Minesweeper/main.cpp
@@ -0,0 +1,25 @@
+//StringUtils is from my game
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Utils/StringUtils.h"
+#include "Utils/FileUtils.h"
+#include "Utils/ConsoleUtils.h"
+
+using std::string;
+using std::vector;
+
+int main()
+{
+	string in;
+	//in = Utils::readFileAsText("out.txt");
+	in = Utils::readInput();
+	vector<string> splitIn = Utils::splitString(in,'\n');
+	
+	for(auto& str: splitIn)
+	{
+		std::cout<<str<<std::endl;
+	}
+	return EXIT_SUCCESS;
+}
