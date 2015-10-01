a29078eb8d5f11e4d3b5d8ee783b494a4334d362
James Felts <jfelts1@users.noreply.github.com>
2015-10-01 03:27:38 -0700

    fixed the RNG

--- Assign2Minesweeper/GenerateMines/GenerateMines.cpp
+++ Assign2Minesweeper/GenerateMines/GenerateMines.cpp
@@ -19,9 +19,11 @@ void GenerateMines::generateField(const int x, const int y)
 
 	//about 10% mines
 	std::uniform_int_distribution<>rand(0, 9);
+	std::random_device rd;
 	std::mt19937 gen;
 	for (auto& i : m_mineField)
 	{
+		gen.seed(rd());
 		for (auto vec : i)
 		{
 			vec = !static_cast<bool>(rand(gen));
--- Assign2Minesweeper/GenerateMines/main.cpp
+++ Assign2Minesweeper/GenerateMines/main.cpp
@@ -9,7 +9,7 @@ int main()
 	GenerateMines mines;
 	
 	std::ofstream fout;
-	fout.open("out.txt", fout.out);
+	fout.open("in.txt", fout.out);
 	for (int i = ARRAY_MAX;i >=0;i--)
 	{
 		for (int j = ARRAY_MAX;j >=0;j--)
